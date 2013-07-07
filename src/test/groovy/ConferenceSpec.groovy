80e6b627707a8640b6382e885d3c9aa9189edd33
advancedplawler <plawler@advanc-ed.org>
2013-07-07 13:47:20 -0400

    starting on conference scheduling

---
+++ src/test/groovy/ConferenceSpec.groovy
@@ -0,0 +1,50 @@
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: plawler
+ * Date: 7/7/13
+ * Time: 1:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ConferenceSpec extends Specification {
+
+    @Shared talks = [] as Set
+    def setupSpec() {
+        def rawTalks = ["Writing Fast Tests Against Enterprise Rails 60min",
+                "Overdoing it in Python 45min",
+                "Lua for the Masses 30min",
+                "Ruby Errors from Mismatched Gem Versions 45min",
+                "Common Ruby Errors 45min",
+                "Rails for Python Developers lightning",
+                "Communicating Over Distance 60min",
+                "Accounting-Driven Development 45min",
+                "Woah 30min",
+                "Sit Down and Write 30min",
+                "Pair Programming vs Noise 45min",
+                "Rails Magic 60min",
+                "Ruby on Rails: Why We Should Move On 60min",
+                "Clojure Ate Scala (on my project) 45min",
+                "Programming in the Boondocks of Seattle 30min",
+                "Ruby vs. Clojure for Back-End Development 30min",
+                "Ruby on Rails Legacy App Maintenance 60min",
+                "A World Without HackerNews 30min",
+                "User Interface CSS in Rails Apps 30min"]
+
+        rawTalks.each { raw ->
+            talks.add(SchedulableFactory.createTalk(raw))
+        }
+    }
+
+    def "conference builds a track"() {
+        setup:
+        Track track = new Track.Builder(talks).morning().build()
+
+        expect:
+        println talks.size()
+        println track.morning().size()
+    }
+
+
+}
